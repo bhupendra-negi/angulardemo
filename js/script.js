44a9473dc179f19cf7ad5b370c550d68efa0cc9f
bhupendra-negi <bhupendra.negi1011@gmail.com>
2016-03-23 00:01:43 +0530

    added simple ng-repeat

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
 // this module needs to be refrences in html page
-var myApp  = angular.module("myModule",[]);
+var myApp = angular.module("myModule", []);
 // attaching controller to create momdel for view to show it
-myApp.controller("myController",function($scope)
-{
+myApp.controller("myController", function($scope) {
   $scope.message = "My first Angular Model";
   // object in Model
   var employee = {
-    firstName : "Bhupendra",
-    secondName : "Negi"
+    firstName: "Bhupendra",
+    secondName: "Negi"
   };
   $scope.employee = employee;
 });
 
 // another controller to load images with ng-src directive
-myApp.controller("ImgController", function($scope){
+myApp.controller("ImgController", function($scope) {
+  var country = {
+    name: "INDIA",
+    capital: "New Delhi",
+    image: "images/flag.png"
+  };
+  // attaching country model to $scope.
+  $scope.country = country;
+});
 
-var country  = {
-  name:"INDIA",
-  capital:"New Delhi",
-  image:"images/flag.png"
-};
-// attaching country model to $scope.
-$scope.country = country;
+// below function adds simple ng-repeat controller
+myApp.controller("RepeatController", function($scope) {
+
+  var friends = [{
+    name: "bhupendra",
+    city: "doon"
+  }, {
+    name: "sid",
+    city: "srinagar"
+  }, {
+    name: "shubham",
+    city: "gurgoan"
+  }];
+
+  $scope.friends = friends;
+
 });
